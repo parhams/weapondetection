a4ebbf576933a16eac8ce7d260977f5058c65961
parhams <samani.m@gmail.com>
2020-07-07 13:40:20 +0430

    added  make video from images

---
+++ make_video.py
@@ -0,0 +1,16 @@
+import cv2
+import glob
+
+img_array = []
+size = (0, 0)
+for filename in glob.glob('videos/save2/*.jpg'):
+    img = cv2.imread(filename)
+    height, width, layers = img.shape
+    size = (width, height)
+    img_array.append(img)
+
+out = cv2.VideoWriter('videos/video2.avi', cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+
+for i in range(len(img_array)):
+    out.write(img_array[i])
+out.release()
